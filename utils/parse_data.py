af1dc0eec1e9a804d4ac0bbbdcc95d832d772178
Benjamin Elder <bentheelder@gmail.com>
2016-09-30 14:13:21 -0400

    track individual days

--- utils/parse_data.py
+++ utils/parse_data.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python
-import os, sys, csv
+from __future__ import print_function
+import os, sys, csv, datetime
 
 # object to hold parsed data for a dog
 class DogData(object):
+    class DayData(object):
+        def __init__(self, day):
+            self.day = day
+            self.awake = None
+            self.active = None
+            self.rest = None
+
+        def __repr__(self):
+            return "<DogData.DayData day:%s awake:%s active:%s rest:%s>" % \
+                (self.day, self.awake, self.active, self.rest)
+
     def __init__(self, name, dog_id = None, tattoo_number = None):
         self.name = name
         self.dog_id = dog_id
         self.tattoo_number = tattoo_number
+        self.days = {}
         self.awake_total = 0
         self.active_total = 0
         self.rest_total = 0
@@ -18,13 +31,14 @@ class DogData(object):
         """
 
     def __repr__(self):
-        return "<DogData name:%s dog_id:%s tattoo_number:%s awake:%s active:%s rest:%s>" % \
-            (self.name, self.dog_id, self.tattoo_number,\
+        return "<DogData name:%s dog_id:%s tattoo_number:%s days:%s awake:%s active:%s rest:%s>" % \
+            (self.name, self.dog_id, self.tattoo_number, self.days,\
             self.awake_total, self.active_total, self.rest_total)
 
 # method to parse a "cci-puppy_minutes-*.csv" file
-def parse_minutes_file(file_name, variable_name, data):
+def parse_minutes_file(file_name, minutes_type, data):
     with open(file_name, 'rb') as csv_file:
+        total_name = minutes_type+"_total"
         # row number
         row_n = 0
         # this tracks the order of the dogs by column 1..n
@@ -54,13 +68,18 @@ def parse_minutes_file(file_name, variable_name, data):
                                 int(float(tattoo_number))
             # remaining rows are dates with minutes
             else:
+                parts = row[0].split('-')
+                day = datetime.date(int(parts[0]), int(parts[1]), int(parts[2]))
                 for i in range(1, len(row)):
                     raw = row[i]
                     if raw != "n/a":
                         minutes = int(float(raw))
                         dog = data[dog_order[i-1]]
-                        curr_minutes = getattr(dog, variable_name)
-                        setattr(dog, variable_name, curr_minutes + minutes)
+                        curr_minutes = getattr(dog, total_name)
+                        setattr(dog, total_name, curr_minutes + minutes)
+                        if not (day in dog.days):
+                            dog.days[day] = DogData.DayData(day)
+                        setattr(dog.days[day], minutes_type, minutes)
             row_n += 1
     return data
 
@@ -70,9 +89,9 @@ def parse_dog_data(data_dir):
     active_path = os.path.join(data_dir, "cci-puppy_minutes-active.csv")
     rest_path = os.path.join(data_dir, "cci-puppy_minutes-rest.csv")
     data = {}
-    parse_minutes_file(awake_path, "awake_total", data)
-    parse_minutes_file(active_path, "active_total", data)
-    parse_minutes_file(rest_path, "rest_total", data)
+    parse_minutes_file(awake_path, "awake", data)
+    parse_minutes_file(active_path, "active", data)
+    parse_minutes_file(rest_path, "rest", data)
     return data
 
 # data print utility
@@ -100,8 +119,8 @@ if __name__ == "__main__":
     # load the data from the files
     dog_data = parse_dog_data(data_dir)
     # sort dogs by activity
-    by_activity = sorted((v.active_total, v.name, v) for v in dog_data.values())
+    by_activity = sorted((v.active_total, v.name, v.dog_id) for v in dog_data.values())
     # print sorted by activity, most first
-    print("active\t\tname\t\trepr(DogData)")
+    print("active\t\tname\t\tdog_id")
     print_bar()
     iterate_and_print(by_activity[::-1], 2)
