98ae93ca3936c14476d8bd3f3d21f2d21a6aa533
brandenbrown <brandenbrown@lawn-128-61-37-65.lawn.gatech.edu>
2017-02-28 18:51:56 -0500

    fixed padding on both sides of the page and corrected the issue
    fixed padding on both sides of the page and corrected the issue where
    the pop up details on the graph still said minutes instead of percent

--- static/index.js
+++ static/index.js
@@ -759,7 +759,7 @@ function createChartEight() {
         tooltip: {
             headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
             pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-            '<td style="padding:0"><b>{point.y:.0f} mins</b></td></tr>',
+            '<td style="padding:0"><b>{point.y:.0f} percent</b></td></tr>',
             footerFormat: '</table>',
             shared: true,
             useHTML: true
